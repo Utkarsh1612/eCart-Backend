bb675d0ec7cf3c11830936eb70a6416845b3924a
Utkarsh Tyagi <utkarshtyagi1612@gmail.com>
2025-08-21 15:49:07 +0530

    added more products in database

--- index.js
+++ index.js
@@ -18,24 +18,80 @@ app.use(cors(corsOptions));
 
 initialiseDatabase();
 
-const newProduct = {
-  image:
-    "https://m.media-amazon.com/images/I/71q9o1FGvDL._AC_UL480_FMwebp_QL65_.jpg",
-  name: "HEALTH SAFE",
-  price: 699,
+const newProducts = [
+ {
+  image: "https://m.media-amazon.com/images/I/61Hg56EKC3L._AC_UL480_FMwebp_QL65_.jpg",
+  name: "Impakto",
+  price: 999,
   inStock: true,
-};
+ },
+ {
+  image: "https://m.media-amazon.com/images/I/51GOpp8rAJL._AC_UL480_FMwebp_QL65_.jpg",
+  name: "Puma",
+  price: 1599,
+  inStock: true,
+ },
+ {
+  image: "https://m.media-amazon.com/images/I/51f8-BLfiNL._AC_UL480_FMwebp_QL65_.jpg",
+  name: "AADI",
+  price: 498,
+  inStock: true,
+ },
+ {
+  image: "https://m.media-amazon.com/images/I/71cflgAolqL._AC_UL480_FMwebp_QL65_.jpg",
+  name: "ASIAN",
+  price: 769,
+  inStock: true,
+ },
+ {
+  image: "https://m.media-amazon.com/images/I/81umQJztTxL._AC_UL480_FMwebp_QL65_.jpg",
+  name: "RXN",
+  price: 1349,
+  inStock: true,
+ },
+ {
+  image: "https://m.media-amazon.com/images/I/61Z8kRV-HCL._AC_UL480_FMwebp_QL65_.jpg",
+  name: "CHILWELL",
+  price: 499,
+  inStock: true,
+ },
+ {
+  image: "https://m.media-amazon.com/images/I/71V-00jKq1L._AC_UL480_FMwebp_QL65_.jpg",
+  name: "Vector X",
+  price: 1465,
+  inStock: true,
+ },
+ {
+  image: "https://m.media-amazon.com/images/I/51JZ0OZlIGL._AC_UL480_FMwebp_QL65_.jpg",
+  name: "Nike",
+  price: 15295,
+  inStock: true,
+ }
 
-const createProduct = async (newProduct) => {
-  try {
-    const product = new Product(newProduct);
-    const createdProduct = await product.save();
-  } catch (error) {
-    console.log("Error in creating product:", error);
+];
+
+// const createProduct = async (newProduct) => {
+//   try {
+//     const product = new Product(newProduct);
+//     const createdProduct = await product.save();
+//   } catch (error) { 
+//     console.log("Error in creating product:", error);
+//   }
+// };
+
+const createdProducts = async (newProducts) => {
+  try{  
+    for (const element of newProducts){
+      const product = new Product(element);
+      const createdProduct = await product.save();
+    }
+  }catch(error){
+    console.log("Error in adding products", error.message);
   }
-};
+}
 
-//createProduct(newProduct);
+//createdProducts(newProducts);
+//createProduct(newProduct); 
 
 const fetchAllProducts = async () => {
   try {
